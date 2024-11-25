bf339643b0aaaec4ccb9cde3dc47c7f9be1c5713
arthurconde695 <111357089+arthurconde695@users.noreply.github.com>
2024-11-25 11:45:19 -0300

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [],
+  template: `
+    <h1>Hello world!</h1>
+  `,
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'angular';
+}
+
+
